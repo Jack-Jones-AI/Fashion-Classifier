16b42f995a196cca5982b1b1d247ba6bc026c57b
fistadev <fabiofistarol@gmail.com>
2021-05-31 10:28:53 +0200

    Update mainscript.py

---
+++ mainscript.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+from sklearn.metrics import confusion_matrix
