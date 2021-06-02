a9cd086b4999e10899e84b1f4bfa96c9fca829df
aimwps <mwpskinner@gmail.com>
2021-06-02 14:49:31 +0100

    changed tester file

--- JJ_tester.py
+++ JJ_tester.py
@@ -16,6 +16,30 @@ MODEL = torch.load("trained_models/trained_model.pt")
 
 
 
+def imshow(image, ax=None, title=None, normalize=True):
+    """Imshow for Tensor."""
+    if ax is None:
+        fig, ax = plt.subplots()
+    image = image.numpy().transpose((1, 2, 0))
+
+    if normalize:
+        mean = np.array([0.485, 0.456, 0.406])
+        std = np.array([0.229, 0.224, 0.225])
+        image = std * image + mean
+        image = np.clip(image, 0, 1)
+
+    ax.imshow(image)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.tick_params(axis='both', length=0)
+    ax.set_xticklabels('')
+    ax.set_yticklabels('')
+
+    return ax
+
+
 def output_label(label):
     output_mapping = {
                  0: "T-shirt/Top",
@@ -36,10 +60,9 @@ def output_label(label):
 IMG_DIR = './img_test/shoe.png'
 loader = transforms.Compose([
                             transforms.Resize((28,28)),
-                            transforms.ToTensor(),
-                            transforms.Normalize((0.5), (0.5))
-                              ])
-
+                            transforms.ToTensor(),])
+                            #transforms.Normalize((0.5), (0.5))
+                              #])
 
 def image_loader(image_name):
     image = Image.open(image_name)
@@ -50,6 +73,10 @@ def image_loader(image_name):
 
 ti = image_loader(IMG_DIR)
 
+MODEL.eval()
 output = MODEL(ti)
+tensor, label = torch.max(output, 1)
 
+print(output_label(label.item()))
+#imshow(tensor)
 print(output)
